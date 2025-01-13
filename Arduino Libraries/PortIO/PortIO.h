0b02217534d23a15e95ba5373132ad6fde30a705
Craig Gregory <92106030+craig-jonathan-gregory@users.noreply.github.com>
2025-01-13 09:32:08 -0600

    Testing new Git Merge/Deploy script

--- Arduino Libraries/PortIO/PortIO.h
+++ Arduino Libraries/PortIO/PortIO.h
@@ -4,20 +4,10 @@
 #define   PortIO_h_
 
 
-#define   BIT0          _BV(0)
-#define   BIT1          _BV(1)
-#define   BIT2          _BV(2)
-#define   BIT3          _BV(3)
-#define   BIT4          _BV(4)
-#define   BIT5          _BV(5)
-#define   BIT6          _BV(6)
-#define   BIT7          _BV(7)
-
-
 #define   pio_PortAsInput             0x00
 #define   pio_PortAsOutput            0xFF
 
-#define   pio__PinToPinID(pin)        pio__##pin##_PinID
+#define   pio__PinToPinNum(pin)       pio__##pin##_PinNum
 #define   pio__PinToBIT(pin)          pio__##pin##_BIT
 #define   pio__PinToDDR(pin)          pio__##pin##_DDR
 #define   pio__PinToPIN(pin)          pio__##pin##_PIN
@@ -29,7 +19,7 @@
 // #define   pio_pulsePin(pin)           ()
 #define   pio_setPinAsOutput(pin)     ( pio__PinToDDR(pin) |= pio__PinToBIT(pin) )
 #define   pio_setPinAsInput(pin)      ( pio__PinToDDR(pin) &= ~pio__PinToBIT(pin) )
-// #define   pio_setPinAsInput_wPullup(pin)    ()
+// #define   pio_setPinAsInput_wPullup(pin)    ( pio__PinToDDR(pin) |= pio__PinToBIT(pin) )
 
 #define   pio_attachInterruptToPin(pin, edge, handler)   ( attachInterrupt(digitalPinToInterrupt(pio__PinToPinNum(pin)), handler, edge) )
 
@@ -41,6 +31,5 @@
 #define   pio_readPort(port)          ( pio__PortToPIN(port) )
 #define   pio_writePort(port, data)   ( pio__PortToPORT(port) = data )
 #define   pio_setPortIO(port, dir)    ( pio__PortToDDR(port) = dir )
-
 
 #endif
--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Retro-Computing
 
-## Arduino Libraries - Utils for Arduino coding
+### Arduino Libraries - Utils for Arduino coding
 
-## RetroShield 6502 - Utils to debug RetroShield by 8-Bit Force 
+### RetroShield 6502 - Utils to debug RetroShield by 8-Bit Force 
 
-## SBC6502 - Single Board Computer project for 6502 processors
+### SBC6502 - Single Board Computer project for 6502 microprocessors
 
-## xa - 6502 processor family cross assembler and utils
+### xa - 6502 microprocessor family cross assembler and utils
